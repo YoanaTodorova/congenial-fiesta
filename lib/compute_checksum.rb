a064b31d2d50ffc4ab9bc67e13ed98866dcd2007
Yoana Todorova <yoana.todorova@fundingcircle.com>
2017-10-28 11:34:35 +0300

    Implement methods for modifications of string; specs

--- lib/compute_checksum.rb
+++ lib/compute_checksum.rb
@@ -16,12 +16,20 @@ class ComputeChecksum
     apply_modifications_to_string
 
     {
-      count_of_original_words: ,
-      count_of_newly_created_words: ,
-      count_of_upper_case_vowels: ,
-      count_of_consonants: ,
-      length_of_original_string: ,
+      count_of_original_words: selected_substrings_count.call(original_string, WORD_REGEXP),
+      count_of_newly_created_words: selected_substrings_count.call(modified_string, WORD_REGEXP),
+      count_of_upper_case_vowels: selected_substrings_count.call(modified_string, UPPER_CASE_VOWELS_REGEXP),
+      count_of_consonants: selected_substrings_count.call(modified_string, CONSONANTS_REGEXP),
+      length_of_original_string: original_string.length,
     }
+  end
+
+  WORD_REGEXP = /[a-zA-Z]+/
+  UPPER_CASE_VOWELS_REGEXP = /[AEIOU]/
+  CONSONANTS_REGEXP = /[b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z]/
+
+  def selected_substrings_count
+    lambda { |string, regexp| string.scan(regexp).count }
   end
 
   def apply_modifications_to_string
@@ -29,5 +37,30 @@ class ComputeChecksum
     split_into_limited_characters_strings
     capitalize_each_word
     upcase_vowel_after_consonants_and_upcase_vowel
+  end
+
+  def remove_non_english_alphabet_characters
+    @modified_string.gsub!(/[^a-zA-Z ]/, '')
+  end
+
+  def split_into_limited_characters_strings(word_length: 10)
+    @modified_string = @modified_string.gsub(/ /, '').scan(/.{1,#{word_length}}/).join(' ')
+  end
+
+  def capitalize_each_word
+    @modified_string.gsub!(/\b[a-z]/, &:upcase)
+  end
+
+  CONSONANTS = 'b-df-hj-np-tv-zB-DF-HJ-NP-TV-Z'
+  VOWELS = 'aeiouAEIOU'
+
+  def upcase_vowel_after_consonants_and_upcase_vowel(number_of_preceeding_consonants: 2)
+    regexp = /(?<=[AEIOU])(?:[^VOWELS]*?)(?<=[#{CONSONANTS}]{2})([aeiou])/
+
+    while regexp.match @modified_string
+      @modified_string.gsub!(regexp) { |match| match.gsub(/\w$/, &:upcase) }
+    end
+
+    @modified_string
   end
 end
---
+++ spec/lib/compute_checksum_spec.rb
@@ -0,0 +1,93 @@
+require './lib/compute_checksum'
+
+RSpec.describe ComputeChecksum do
+  describe '.call' do
+    context 'example 1' do
+      let(:expected) do
+        {
+          count_of_original_words: 7,
+          count_of_newly_created_words: 4,
+          count_of_upper_case_vowels: 5,
+          count_of_consonants: 21,
+          length_of_original_string: 37,
+        }
+      end
+      let(:string) { 'foo bar baz wibble fizzbuzz fizz buzz' }
+      subject { described_class.call(string: string) }
+
+      it 'returns a hash of all checksums' do
+        expect(subject).to eq(expected)
+      end
+    end
+
+    context 'example 2' do
+      let(:expected) do
+        {
+          count_of_original_words: 9,
+          count_of_newly_created_words: 4,
+          count_of_upper_case_vowels: 3,
+          count_of_consonants: 24,
+          length_of_original_string: 43,
+        }
+      end
+      let(:string) { 'The quick brown fox jumps over the lazy dog' }
+      subject { described_class.call(string: string) }
+
+      it 'returns a hash of all checksums' do
+        expect(subject).to eq(expected)
+      end
+    end
+  end
+
+  describe '#remove_non_english_alphabet_characters' do
+    let(:string) { 'здравейте The fiv5e boxing wizards jump quick#ly' }
+    let(:expected) { ' The five boxing wizards jump quickly' }
+    subject { described_class.new(string: string).remove_non_english_alphabet_characters }
+
+    it 'removes characters that do not belong to the English alphabet' do
+      expect(subject).to eq(expected)
+    end
+  end
+
+  describe '#split_into_limited_characters_strings' do
+    let(:string) { 'The five boxing wizards jump quickly' }
+    let(:expected) { 'Thefivebox ingwizards jumpquickl y' }
+    subject { described_class.new(string: string).split_into_limited_characters_strings }
+
+    it 'creates words which are 10 characters long' do
+      expect(subject).to eq(expected)
+    end
+  end
+
+  describe '#capitalize_each_word' do
+    let(:string) { 'The five    boxing wizards jump quickly' }
+    let(:expected) { 'The Five    Boxing Wizards Jump Quickly' }
+    subject { described_class.new(string: string).capitalize_each_word }
+
+    it 'capitalizes each word' do
+      expect(subject).to eq(expected)
+    end
+  end
+
+  describe '#upcase_vowel_after_consonants_and_upcase_vowel' do
+    subject { described_class.new(string: string).upcase_vowel_after_consonants_and_upcase_vowel }
+
+    context 'when preceeding consonants are next to each other' do
+      let(:string) { 'Thequickbr Ownfoxjump Soverthela Zydog' }
+      let(:expected) { 'Thequickbr OwnfOxjUmp Soverthela Zydog' }
+
+      it 'upcases the vowel' do
+        expect(subject).to eq(expected)
+      end
+    end
+
+    context 'when preceeding consonants are separated by a whitespace' do
+      let(:string) { 'childrEn do need freedom' }
+      let(:unexpected) { 'childrEn dO need freedom' }
+
+      it 'does not upcase the vowel' do
+        expect(subject).not_to eq(unexpected)
+      end
+    end
+  end
+end
